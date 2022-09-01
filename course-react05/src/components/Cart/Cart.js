ba3f3a520bfab0ea03d4d5fd55a911e8178dc105
Kevin Breurken <kevinbreurken@gmail.com>
2022-09-01 18:05:30 +0200

    fixed typo in code

--- course-react05/src/components/Cart/Cart.js
+++ course-react05/src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
-			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].maps(
+			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
 				(item) => (
 					<li>{item.name}</li>
 				)
